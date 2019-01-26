7fafd6813956c844b5fe0d885dac9d8478279449
Liflex <a.dmitriev@opinionsn.com>
2019-01-26 22:40:21 +0300

    инициализация бд

--- src/main/java/com/javathon/backend/BackendApplication.java
+++ src/main/java/com/javathon/backend/BackendApplication.java
@@ -2,8 +2,15 @@ package com.javathon.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
+@EnableTransactionManagement
+@EnableWebSecurity
+@EnableCaching
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
