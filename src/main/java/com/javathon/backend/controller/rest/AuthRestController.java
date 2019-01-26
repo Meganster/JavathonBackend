2a4e88c8f06a048c0d53513b9705ced5c722449f
supreme2302 <chuvashov.egor@mail.ru>
2019-01-27 00:49:13 +0300

    -

--- src/main/java/com/javathon/backend/controller/rest/AuthRestController.java
+++ src/main/java/com/javathon/backend/controller/rest/AuthRestController.java
@@ -2,16 +2,13 @@ package com.javathon.backend.controller.rest;
 
 import com.javathon.backend.model.User;
 import com.javathon.backend.service.dto.UserDTO;
-<<<<<<< HEAD
+
 import com.javathon.backend.util.UserConverter;
-=======
 import com.javathon.backend.service.interf.UserService;
->>>>>>> ef6456f03ae9a5ae536a725bcf9791ac87512f46
 import com.javathon.backend.util.validators.AuthValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
