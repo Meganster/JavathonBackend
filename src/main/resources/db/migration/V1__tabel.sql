282065e638deead74e6ea31789f7eb34ce37cd0f
supreme2302 <chuvashov.egor@mail.ru>
2019-01-26 20:05:24 +0300

    sql

---
+++ src/main/resources/db/migration/V1__tabel.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY ,
+  imei INTEGER NOT NULL ,
+  username citext,
+  last_latitude DOUBLE ,
+  last_longtitude DOUBLE ,
+  is_visible boolean default false
+);
